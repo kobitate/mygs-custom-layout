761492ac62ddc4a3f950c7d95b56aa63a0a7044f
GitCub <gitcub@gmail.com>
2017-05-18 16:14:03 -0400

    Final progress on 1.0

--- main/main.js
+++ main/main.js
@@ -1,10 +1,25 @@
+var customLayout;
+
+function init() {
+	chrome.storage.sync.get("portal-layout", function(data) {
+		if (data["portal-layout"] == undefined) {
+			console.log("Initializing MyGS Custom Layout");
+			console.log(data);
+			saveLayout();
+		}
+		else {
+			console.log(data);
+			$.each(data["portal-layout"], function(columnIndex, column) {
+				$.each(column, function(channelIndex, channel) {
+					var channelPanel = $('.channel-id-'+ channel);
+					channelPanel.appendTo($(".portal-column").get(columnIndex));
+				});
+			});
+		}
+	});
+}
+
 function prepPage() {
-	$.get(chrome.extension.getURL('main/editBar.html'), function(data) {
-		$("body").append(data);
-	});
-	$.get(chrome.extension.getURL('main/editButton.html'), function(data) {
-		$("#edit-start-controls").prepend(data);
-	});
 	$(".col-sm-6.col-md-3").addClass("portal-column");
 	$(".panel.panel-primary").each(function() {
 		$(this).addClass("channel");
@@ -12,18 +27,54 @@ function prepPage() {
 
 		if (channel.length > 0) {
 			var split = channel.attr("id").split("_");
-			$(this).attr("id", "portal-channel-" + split[1]);
+			$(this).addClass("channel-id-" + split[1]);
+			$(this).data("channel-id", split[1]);
 		}
 		else {
 			var title = $(this).find("h2").text();
 			if (title == "My Messages & Alerts") {
-				$(this).attr("id", "portal-channel-msg");
+				$(this).addClass("channel-id-msg");
+			$(this).data("channel-id", "msg");
 			}
 		}
 	});
-	// get rid of their dumb google analytics code that just creates more problems than it solves
-	$("a").unbind("click");
+
+	init();
 }
+
+function getLayout() {
+	// first column
+	var col = 0;
+
+	// object to build new layout
+	var newLayout = {};
+
+	// loop through columns
+	$(".portal-column").each(function(){
+		var colIndex = String(col);
+		// prepare array of channel id's
+		newLayout[colIndex] = [];
+		// loop through channels in column
+		$(this).find(".channel").each(function(){
+			// channel number from channel-outer div
+			var channelID = $(this).data("channel-id");
+
+			// add channel number to array
+			newLayout[colIndex].push(channelID);
+		});
+		// prepare for next column
+		col++;
+	});
+
+	// return the object we built
+	return newLayout;
+}
+
+function saveLayout() {
+	var layout = getLayout();
+	chrome.storage.sync.set({"portal-layout": layout}, function() {});
+}
+
 
 prepPage();
 
@@ -31,9 +82,8 @@ $(".portal-column").sortable({
 	items: ".channel",
 	connectWith: ".portal-column",
 	handle: ".panel-heading",
+	stop: function() {
+		saveLayout();
+	},
 	revert: 200
-});
-
-$('#folioTerm').on('change', function(event) {
-	alert("Hey there! This is a message from the MyGS Custom Layout Chrome Extension. Unfortunately, the extension currently breaks the Folio semester dropdown. But no worries, just refresh the page once you've picked your semester and it will display.");
 });
